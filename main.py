5564be6fe1ab04997cda7029479623496ddca421
vyomshah05 <vyomshah005@gmail.com>
2026-02-26 15:33:55 -0800

    edited main to take comand line args

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
 from virtual_memory import *
 
 if __name__ == '__main__':
-    run()
-    run(init_path='init_dp.txt', va_path='input_dp.txt', out_path='output_dp.txt', demand_paging=True)
+
+    init_path = sys.argv[1]
+    va_path = sys.argv[2]
+    out_path = sys.argv[3]
+
+    demand_arg = sys.argv[4].lower()
+    if demand_arg == "true":
+        demand_paging = True
+    elif demand_arg == "false":
+        demand_paging = False
+
+    run(
+        init_path=init_path,
+        va_path=va_path,
+        out_path=out_path,
+        demand_paging=demand_paging
+    )
